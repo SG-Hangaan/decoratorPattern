9cf0836ee753247a9c734ac8026f9793578438bb
Sharon Grace T. Hangaan <127215110+SG-Hangaan@users.noreply.github.com>
2023-12-08 15:51:45 +0800

    update syntax, fix interface, BankAccount

---
+++ DecoratorPattern/BankAccount.java
@@ -0,0 +1,11 @@
+package DecoratorPattern;
+
+public interface BankAccount {
+   
+    String showAccountType();
+    Double getInterestRate();
+    Double getBalance();
+    String showBenefits();
+    Double computeBalanceWithInterest();
+    String showInfo();
+}
